bc7c78ac3f948cd70c7bde1e6284f3bc2ab72985
guo <yang.guo@chinaredstar.com>
2016-05-04 11:08:59 +0800

    项目结构完善

--- modules/hydra-manager/src/main/java/com/jd/bdp/hydra/manager/Main.java
+++ modules/hydra-manager/src/main/java/com/jd/bdp/hydra/manager/Main.java
@@ -1,8 +1,5 @@
 package com.jd.bdp.hydra.manager;
 
-/**
- * User: xiangkui Date: 13-5-16 Time: 下午3:29
- */
 public class Main {
     public static void main(String[] args) {
         com.alibaba.dubbo.container.Main.main(args);
