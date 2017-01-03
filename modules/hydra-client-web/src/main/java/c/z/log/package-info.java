158e92d27f327a75fee69b68f62366f3fbcc538c
zhuzhong <dacong@j1.com>
2017-01-03 17:55:33 +0800

    增加log注解

---
+++ modules/hydra-client-web/src/main/java/c/z/log/package-info.java
@@ -0,0 +1,15 @@
+
+/** 通过注解的形式的为生成相应的方法log
+ * @author sunff
+ *
+ */
+package c.z.log;
+
+/**
+使用方法
+在spring配置文件中
+1.打开   <aop:aspectj-autoproxy />
+
+2.注入bean MethodLogAspect
+3.在需要打印log的方法使用注解MethodLog
+*/
